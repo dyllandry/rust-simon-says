a4582fd4bd268f8b849732ca0fc9372d84510fc2
Dylan Landry <dylanglandry@gmail.com>
2022-03-24 17:11:59 -0400

    add ecs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+mod ecs;
+
 fn main() {
-    println!("Hello, world!");
+    let mut world = ecs::World::new();
+
+    let entity_1 = world.new_entity();
+    let name = ecs::Name("Dylan");
+    world.add_component_to_entity(entity_1, name);
+
+    let mut names = world.borrow_component_vec::<ecs::Name>().unwrap();
+    for name in names.iter_mut().filter_map(|name| name.as_mut()) {
+        println!("Name ({})", name.0);
+    }
 }
