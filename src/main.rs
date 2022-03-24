8db72546fce30d833228c5e8d68909574416a895
Dylan Landry <dylanglandry@gmail.com>
2022-03-24 17:25:21 -0400

    add glium event loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+// #[macro_use]
+extern crate glium;
+
 mod ecs;
 
 fn main() {
@@ -11,4 +14,32 @@ fn main() {
     for name in names.iter_mut().filter_map(|name| name.as_mut()) {
         println!("Name ({})", name.0);
     }
+
+    use glium::{glutin, Surface};
+
+    let event_loop = glutin::event_loop::EventLoop::new();
+    let wb = glutin::window::WindowBuilder::new();
+    let cb = glutin::ContextBuilder::new();
+    let display = glium::Display::new(wb, cb, &event_loop).unwrap();
+
+    event_loop.run(move |ev, _, control_flow| {
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 1.0, 1.0);
+        target.finish().unwrap();
+
+        let next_frame_time =
+            std::time::Instant::now() + std::time::Duration::from_nanos(16_666_667);
+        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
+
+        match ev {
+            glutin::event::Event::WindowEvent { event, .. } => match event {
+                glutin::event::WindowEvent::CloseRequested => {
+                    *control_flow = glutin::event_loop::ControlFlow::Exit;
+                    return;
+                }
+                _ => return,
+            },
+            _ => return,
+        }
+    });
 }
